427ae9b11d9bb4e5f6388105cb6aa59c85e2524d
parameshwar <parameshwarshiva119@gmail.com>
2024-03-07 12:14:57 +0530

    changed header image

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,7 +21,7 @@ export function Header() {
     return (<>
         <nav className="navbar navbar-expand-lg fixed-top bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#"><img src={logo} alt="logo" className="logo image-fluid" /></a>
+                <a className="navbar-brand" href="/"><img src={logo} alt="logo" className="logo image-fluid" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
